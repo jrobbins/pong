15779de1daa744ccf8a8a4c1f2577e506e5b35bc
Jason Robbins <jrobbins@gmail.com>
2020-11-07 19:08:43 -0800

    Update pong.js

--- pong.js
+++ pong.js
@@ -1,7 +1,8 @@
-// Ball location
+// Ball location                                                                                                                                                       
 let ballX = 400, ballY = 300;
-let dx = 3, dy = 2
-const ballSize = 16;;
+let ballSpeed = 3;
+let ballRadians = 0.1;
+const ballSize = 16;
 
 const board = document.getElementById('board');
 const ball = document.getElementById('ball');
@@ -11,17 +12,18 @@ let paddle1angle = 0;
 const paddle2 = document.getElementById('paddle2');
 let paddle2y = 200;
 let paddle2angle = 0;
-const paddleHeight = 140;
+let paddle1Height = 140;
+let paddle2Height = 140;
 const paddleWidth = 20;
 
-// board boundaries
+// board boundaries                                                                                                                                                    
 const boardRect = board.getBoundingClientRect();
 
 
 document.addEventListener('keydown', idKey);
 
 function idKey(e) {
-  // Player 1 uses WASD keys.
+  // Player 1 uses WASD keys.                                                                                                                                          
   if (e.key == "d") {
     paddle1angle = Math.min(0.25, paddle1angle + 0.02);
     paddle1.style.transform = "rotate(" + paddle1angle + "rad)";
@@ -35,10 +37,10 @@ function idKey(e) {
     paddle1.style.top = paddle1y + "px";
   }
   else if (e.key == "s") {
-    paddle1y = Math.min(boardRect.bottom - 5 - paddleHeight, paddle1y + 10);
+    paddle1y = Math.min(boardRect.bottom - 5 - paddle1Height, paddle1y + 10);
     paddle1.style.top = paddle1y + "px";
   }
-  // Player 2 uses arrow keys
+  // Player 2 uses arrow keys                                                                                                                                          
   else if (e.code == "ArrowRight") {
     paddle2angle = Math.min(0.25, paddle2angle + 0.02);
     paddle2.style.transform = "rotate(" + paddle2angle + "rad)";
@@ -52,7 +54,7 @@ function idKey(e) {
     paddle2.style.top = paddle2y + "px";
   }
   else if (e.code == "ArrowDown") {
-    paddle2y = Math.min(boardRect.bottom - 5 - paddleHeight, paddle2y + 10);
+    paddle2y = Math.min(boardRect.bottom - 5 - paddle2Height, paddle2y + 10);
     paddle2.style.top = paddle2y + "px";
   }
   else {
@@ -72,51 +74,49 @@ function checkCollision(paddleEl, newX, newY) {
 
 
 function moveBallLoop() {
-  // Think about where the ball would be after this movement.
+  // Think about where the ball would be after this movement.                                                                                                          
+  let dx = ballSpeed * Math.cos(ballRadians);
+  let dy = ballSpeed * Math.sin(ballRadians);
   let newX = ballX + dx;
   let newY = ballY + dy;
+  let newBallRadians = ballRadians;
 
-  // Bounce off of each paddle
+  // Bounce off of each paddle                                                                                                                                         
   if (checkCollision(paddle1, newX, newY)) {
-    let ballSpeed = Math.sqrt(dx*dx + dy*dy);
-    let ballRadians = 0;
-    if (dx != 0) {
-      ballRadians = Math.atan(dy / dx);
-    }
-    let newBallRadians = ballRadians + Math.PI - paddle1angle;
-    dx = -ballSpeed * Math.cos(newBallRadians);
-    dy = ballSpeed * Math.sin(newBallRadians);
-    newX = ballX + dx;
-    newY = ballY + dy;
+    newBallRadians = Math.PI - ballRadians + (paddle1angle / 2);
+    ballSpeed = ballSpeed + 0.1;
+    paddle1Height = Math.max(20, paddle1Height - 5);
+    paddle1.style.height = paddle1Height + 'px';
   }
-  if (checkCollision(paddle2, newX, newY)) {
-    let ballSpeed = Math.sqrt(dx*dx + dy*dy);
-    let ballRadians = 0;
-    if (dx != 0) {
-      ballRadians = Math.atan(dy / dx);
-    }
-    let newBallRadians = ballRadians - paddle2angle;
-    dx = -ballSpeed * Math.cos(newBallRadians);
-    dy = ballSpeed * Math.sin(newBallRadians);
-    newX = ballX + dx;
-    newY = ballY + dy;
+  else if (checkCollision(paddle2, newX, newY)) {
+    newBallRadians = Math.PI - ballRadians + (paddle2angle / 2);
+    ballSpeed = ballSpeed + 0.1;
+    paddle2Height = Math.max(20, paddle2Height - 5);
+    paddle2.style.height = paddle2Height + 'px';
   }
 
-  // Bounce off the walls of the board.
-  if (newX < boardRect.left || newX + ballSize > boardRect.right) {
-    dx = -dx;
-    newX = ballX;  // Ball sticks to wall for one turn.
+  // Bounce off the walls of the board.                                                                                                                                
+  else if (newX < boardRect.left || newX + ballSize > boardRect.right) {
+    newBallRadians = Math.PI - ballRadians;
+    ballSpeed = 3;
+    paddle1Height = 140;
+    paddle1.style.height = paddle1Height + 'px';
+    paddle2Height = 140;
+    paddle2.style.height = paddle2Height + 'px';
   }
-  if (newY < boardRect.top || newY + ballSize > boardRect.bottom) {
-    dy = -dy;
-    newY = ballY;  // Ball sticks to wall for one turn.
+  else if (newY < boardRect.top || newY + ballSize > boardRect.bottom) {
+    newBallRadians = 0 - ballRadians;
   }
 
-  // Once we have computed the new position, save that and update the elements.
-  ballX = newX;
-  ballY = newY;
+  // Once we have computed the new angle, save that and update the elements.                                                                                           
+  ballRadians = newBallRadians;
+  dx = ballSpeed * Math.cos(ballRadians);
+  dy = ballSpeed * Math.sin(ballRadians);
+  ballX = ballX + dx;
+  ballY = ballY + dy;
   ball.style.left = newX + "px";
   ball.style.top = newY + "px";
 }
 
 setInterval(moveBallLoop, 25);
+
